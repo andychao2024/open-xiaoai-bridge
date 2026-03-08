62e2a28ba2fc1120453a114b521339320b1e1f81
coderzc <zhaocong@apache.org>
2026-03-08 15:40:38 +0800

    chore: use git dependency for open-xiaoai client
    Replace local path dependency with git dependency to simplify repo structure.

--- packages/client-rust/target/release/deps/client-9a7ba422d59b8740.d
+++
@@ -1,5 +0,0 @@
-/Users/zc/projects/open-xiaoai/packages/client-rust/target/release/deps/client-9a7ba422d59b8740.d: src/bin/client.rs
-
-/Users/zc/projects/open-xiaoai/packages/client-rust/target/release/deps/client-9a7ba422d59b8740: src/bin/client.rs
-
-src/bin/client.rs:
